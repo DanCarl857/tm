494038bff734fec7b6eb7f63be9848e5078d883e
DanCarl857 <dancarl758@gmail.com>
2025-01-23 16:46:00 +0100

    [fix]: build errors

--- src/ui/components/input.tsx
+++ src/ui/components/input.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import * as React from "react";
 
 import { cn } from "@/utils/css";
